639d8a4bf1922d69c1b0d21809974369b55b4035
Jason Chu <jasonchujc@gmail.com>
2015-01-10 01:10:28 +0900

    Better displayed errors and new standards!
    New standard:
    Forms are to have the names of: <letter of the alphabet in ascending
    order>_<field name>.
    Ex: a_username, b_password.
    In order for errors to be sorted because the are sorted in alphabetical
    order.
    
    New available constant: noErrorMessage which is to be used to create
    errors with only keys (display field feedback) but no messages.
    Example use: c.Validation.Error(noErrorMessage).Key(“b_password”)
    Internally, this will be verified with the added template function of
    “ismessage”.
    Usage:
    {{if ismessage $e}}
        // Display output
    {{end}}
    
    Gonna sleep happy tonight ;)

--- gocode/app/controllers/api.go
+++ gocode/app/controllers/api.go
@@ -1,49 +1,49 @@
 package controllers
 
-import (
-	"github.com/revel/revel"
-	// "convorse/database"
-	"convorse/serialize"
-)
-
-type Api struct {
-	*revel.Controller
-}
-
-func (c Api) Conversation(conversation string) revel.Result {
-	return c.Render()
-}
-
-func (c Api) Unread() revel.Result {
-	return c.Render()
-}
-
-func (c Api) Post() revel.Result {
-	return c.Render()
-}
-
-func (c Api) ConversationUsername() revel.Result {
-
-}
-
-func (c Api) checkDatabase() revel.Result {
-	if !database.DatabaseConnected {
-		go database.Connect()
-		return c.RenderText("database-error")
-	}
-	return nil
-}
-
-func (c Api) checkLogin(username string, password string) revel.Result {
-	if len(username) <= 0 {
-		return c.RenderText("auth-fail", "Username required")
-	}
-	if len(password) <= 0 {
-		return c.RenderText("auth-fail")
-	}
-}
-
-func init() {
-	revel.InterceptMethod(Api.checkDatabase, revel.BEFORE)
-	revel.InterceptMethod(Api.checkLogin, revel.BEFORE)
-}
+// import (
+// 	"github.com/revel/revel"
+// 	"convorse/database"
+// 	// "convorse/serialize"
+// )
+//
+// type Api struct {
+// 	*revel.Controller
+// }
+//
+// func (c Api) Conversation(conversation string) revel.Result {
+// 	return c.Render()
+// }
+//
+// func (c Api) Unread() revel.Result {
+// 	return c.Render()
+// }
+//
+// func (c Api) Post() revel.Result {
+// 	return c.Render()
+// }
+//
+// // func (c Api) ConversationUsername() revel.Result {
+// //
+// // }
+//
+// func (c Api) checkDatabase() revel.Result {
+// 	if !database.DatabaseConnected {
+// 		go database.Connect()
+// 		return c.RenderText("database-error")
+// 	}
+// 	return nil
+// }
+//
+// func (c Api) checkLogin(username string, password string) revel.Result {
+// 	if len(username) <= 0 {
+// 		return c.RenderText("auth-fail", "Username required")
+// 	}
+// 	if len(password) <= 0 {
+// 		return c.RenderText("auth-fail")
+// 	}
+// }
+//
+// func init() {
+// 	revel.InterceptMethod(Api.checkDatabase, revel.BEFORE)
+// 	revel.InterceptMethod(Api.checkLogin, revel.BEFORE)
+// }
--- gocode/app/controllers/lobby.go
+++ gocode/app/controllers/lobby.go
@@ -15,16 +15,19 @@ type Lobby struct {
 // 	fmt.Fprintf(w, "This is a browser!");
 // }
 
-func (c Lobby) LoginAction(username string, password string) revel.Result {
-	accountExists := database.AccountExists(username)
+const noErrorMessage = "no_error_message"
+
+func (c Lobby) LoginAction(a_username string, b_password string) revel.Result {
+	accountExists := database.AccountExists(a_username)
 
 	if accountExists == database.Yes {
-		result := database.VerifyLogin(username, password)
+		result := database.VerifyLogin(a_username, b_password)
 
 		if (result == database.Yes) {
 			return c.RenderText("OK")
 		} else if (result == database.No) {
-			c.Validation.Error("Incorrect passsword!").Key("password")
+			c.Validation.Error("Incorrect password (or username!)").Key("a_username")
+			c.Validation.Error(noErrorMessage).Key("b_password")
 			c.Validation.Keep()
 			c.FlashParams()
 			return c.Redirect("/")
@@ -32,7 +35,7 @@ func (c Lobby) LoginAction(username string, password string) revel.Result {
 			return c.RenderTemplate("errors/database."+c.Request.Format)
 		}
 	} else if accountExists == database.No {
-		c.Validation.Error("That user does not exist!").Key("username")
+		c.Validation.Error("That user does not exist!").Key("a_username")
 		c.Validation.Keep()
 		c.FlashParams()
 		return c.Redirect("/")
@@ -49,30 +52,29 @@ func (c Lobby) Register() revel.Result {
 	return c.Render()
 }
 
-func (c Lobby) RegisterAction(username string, password string, password_again string) revel.Result {
-	c.Validation.MinSize(username, 3).Message("Username must be at least 3 characters long")
-	c.Validation.MaxSize(username, 20).Message("Username cannot be longer than 20 characters")
-	c.Validation.MinSize(password, 5).Message("Password must be at least 5 characters long")
-	c.Validation.MaxSize(password, 50).Message("Password cannot be longer than 50 characters")
-	c.Validation.Required(password == password_again).Message("Passwords aren't the same").Key("password")
+func (c Lobby) RegisterAction(a_username string, b_password string, c_password_again string) revel.Result {
+	c.Validation.MinSize(a_username, 3).Message("Username must be at least 3 characters long")
+	c.Validation.MaxSize(a_username, 20).Message("Username cannot be longer than 20 characters")
+	c.Validation.MinSize(b_password, 5).Message("Password must be at least 5 characters long")
+	c.Validation.MaxSize(b_password, 50).Message("Password cannot be longer than 50 characters")
+	c.Validation.Required(b_password == c_password_again).Message("Passwords aren't the same").Key("b_password")
 
-	if c.Validation.HasErrors() {
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect("/register")
-	}
-
-	exists := database.AccountExists(username)
+	exists := database.AccountExists(a_username)
 
 	if exists == database.No {
-		result := database.RegisterAccount(username, password)
+		if c.Validation.HasErrors() {
+			c.Validation.Keep()
+			c.FlashParams()
+			return c.Redirect("/register")
+		}
+		result := database.RegisterAccount(a_username, b_password)
 		if result == database.Yes {
 			return c.RenderText("Account created!")
 		} else {
 			return c.RenderTemplate("errors/database."+c.Request.Format)
 		}
 	} else if exists == database.Yes {
-		c.Validation.Error("Username already taken!").Key("username")
+		c.Validation.Error("Username already taken!").Key("a_username")
 		c.Validation.Keep()
 		c.FlashParams()
 		return c.Redirect("/register")
@@ -92,4 +94,7 @@ func (c Lobby) checkDatabase() revel.Result {
 
 func init() {
 	revel.InterceptMethod(Lobby.checkDatabase, revel.BEFORE)
+	revel.TemplateFuncs["ismessage"] = func(a *revel.ValidationError) bool {
+		return a.String() != noErrorMessage
+	}
 }
